d42651267301793433aca9e7b34770378cb751ab
randaliang <randaliang@126.com>
2020-06-27 09:57:09 +0800

    Initial commit

---
+++ commonconfig/src/main/java/com/superhope/CommonConfigApplication/CommonConfigApp.java
@@ -0,0 +1,11 @@
+package com.superhope.CommonConfigApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonConfigApp {
+    public static void main( String args[] ){
+        SpringApplication.run(CommonConfigApp.class,args);
+    }
+}
